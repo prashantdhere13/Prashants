cbf64c0522220ad3760d24e7c55440db5a8945cb
prashantdhere13 <78785789+prashantdhere13@users.noreply.github.com>
2022-08-01 12:03:53 +0530

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install srt-tools
+sudo apt-get install net-tools
+sudo nano srtstream.sh
+sudo touch /usr/bin/srtstream.sh
+sudo cp srtstream.sh /usr/bin/srtstream.sh
+sudo chmod +x /usr/bin/srtstream.sh
+sudo nano /etc/systemd/system/srtstream.service
+sudo chmod 644 /etc/systemd/system/srtstream.service
+sudo systemctl start srtstream
+sudo systemctl enable srtstream
+sudo systemctl restart srtstream
