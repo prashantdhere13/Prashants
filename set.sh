8c83b5840b300129487d3928e7228e587868c366
prashantdhere13 <78785789+prashantdhere13@users.noreply.github.com>
2022-12-16 14:00:51 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-sudo apt-get update
-sudo apt-get install srt-tools -y
-sudo apt-get install net-tools -y
-sudo nano srtstream.sh
-sudo touch /usr/bin/srtstream.sh
-sudo cp srtstream.sh /usr/bin/srtstream.sh
-sudo chmod +x /usr/bin/srtstream.sh
-sudo nano /etc/systemd/system/srtstream.service
-sudo chmod 644 /etc/systemd/system/srtstream.service
-sudo systemctl start srtstream
-sudo systemctl enable srtstream
-sudo systemctl restart srtstream
